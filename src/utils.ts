a6a49e3813ed8e33a1013e8af80d501cb8775452
Lars Frölich <lars@li.finance>
2025-04-30 16:18:01 +0200

    use instanceof inside rethrowBecause

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,11 @@ export function notNull<T>(value: T | null): value is T {
 }
 
 /** Rethrow an error with a new message and the original error as the cause */
-export function rethrowBecause(error: Error, reason: string) : never {
-  throw new Error(reason, { cause: error });
+export function rethrowBecause(error: unknown, reason: string) : never {
+  if( error instanceof Error) {
+    throw new Error(reason, {cause: error});
+  }else{
+    // ensure that rethrown Errors are always an instance of Error
+    throw new Error(reason, {cause: new Error('Unknown error: ' + String(error))});
+  }
 }
